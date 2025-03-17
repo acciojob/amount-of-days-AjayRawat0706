d25e533a8a44a74ca104fa9dec5f1cf354d10f50
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-03-17 19:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+daysOfAYear(let x){
+	if(x%100==0){
+		let a=parseInt(x/100);
+		if(a%2==0){
+			return 366;
+		}
+		else{
+			return 365;
+		}
+	}
+	if(x%4==0){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+	
+}
